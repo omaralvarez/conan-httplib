42cf5e1d0b883d325266d55b5d3a006bf7efab8d
Omar Alvarez <omar.alvarez@cinfo.es>
2020-03-12 12:52:27 +0100

    Update to 0.4.0

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 
 class httplibConan(ConanFile):
     name = "httplib"
-    version = "0.3.3"
+    version = "0.4.0"
     description = "C++11 header-only HTTP/HTTPS sever library https://github.com/yhirose/cpp-httplib"
     license = "MIT"
     url = "https://github.com/omaralvarez/conan-httplib"
